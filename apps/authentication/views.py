e1b23103c61338a01469d9619ae5ee4c5af5c711
MarcosCastelo <marcosvitorcastelo@gmail.com>
2024-05-24 09:42:33 -0300

    feature: add filters, pagination and ordering

--- apps/authentication/views.py
+++ apps/authentication/views.py
@@ -1,9 +1,10 @@
-from rest_framework import generics, permissions, viewsets
+from rest_framework import generics, permissions, viewsets, filters
 from rest_framework_simplejwt.views import TokenObtainPairView
 from django.contrib.auth import get_user_model
 from django.contrib.auth.models import Group
 from authentication.serializers import CustomTokenObtainPairSerializer, UserSerializer, UserRegistrationSerializer, GroupSerializer
 from authentication.permissions import IsOwnerOrAdmin
+from django_filters.rest_framework import DjangoFilterBackend
 
 User = get_user_model()
 
@@ -23,12 +24,18 @@ class UserDetailView(generics.RetrieveUpdateAPIView):
     def get_object(self):
         return self.request.user
 
-class UserListView(generics.ListAPIView):
+class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
     permission_classes = [permissions.IsAdminUser]
+    filter_backends = [DjangoFilterBackend, filters.OrderingFilter]
+    filterset_fields = ['username', 'email']
+    ordering_fields = ['username', 'email']
     
 class GroupViewSet(viewsets.ModelViewSet):
     queryset = Group.objects.all()
     serializer_class = GroupSerializer
     permission_classes = [permissions.IsAdminUser]
+    filter_backends = [DjangoFilterBackend, filters.OrderingFilter]
+    filterset_fields = ['name']
+    ordering_fields = ['name']
