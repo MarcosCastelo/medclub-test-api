bbe3800d1bc42b57f12a5fa5351adc13b424cd27
MarcosCastelo <marcosvitorcastelo@gmail.com>
2024-05-24 10:53:54 -0300

    feat//add admin configs

--- apps/authentication/admin.py
+++ apps/authentication/admin.py
@@ -1,16 +1,25 @@
 from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin
-from authentication.models import User
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth.models import Group
+from .models import User
 
-class CustomUserAdmin(UserAdmin):
+@admin.register(User)
+class UserAdmin(BaseUserAdmin):
     fieldsets = (
-        *UserAdmin.fieldsets,
-        (
-            'Additional Info',
-            {
-                'fields': ('phone_number',),
-            }
-        )
+        (None, {'fields': ('username', 'password')}),
+        ('Personal info', {'fields': ('first_name', 'last_name', 'email', 'phone_number')}),
+        ('Permissions', {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
+        ('Important dates', {'fields': ('last_login', 'date_joined')}),
     )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('username', 'password1', 'password2'),
+        }),
+    )
+    list_display = ('username', 'email', 'first_name', 'last_name', 'is_staff')
+    search_fields = ('username', 'first_name', 'last_name', 'email')
+    ordering = ('username',)
 
-admin.site.register(User, CustomUserAdmin)
+admin.site.unregister(Group)
+admin.site.register(Group)
